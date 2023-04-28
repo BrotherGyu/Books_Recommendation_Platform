ae63e49426e48ed29b0eb867dd96185c9a8d30b1
Brother_gyu <brothergyu98@gmail.com>
2023-04-28 11:37:29 +0900

    [update] data_loader.py: docstring add

--- Book_Rec/src/data_loader.py
+++ Book_Rec/src/data_loader.py
@@ -1,3 +1,33 @@
+"""
+This module provides a function for generating a pandas DataFrame containing user information used by the recommender system.
+
+Functions:
+    predict_dataframe_load(age: int, location_country: int, language: int, use_category: bool = False, category: int = None) -> pd.DataFrame:
+        Generates a pandas DataFrame containing user information used by the recommender system.
+        If `use_category` is True, the function generates a DataFrame with an additional column for book categories.
+        Otherwise, the function generates a DataFrame without the book category column.
+        Returns the generated pandas DataFrame.
+        
+Parameters:
+    - age (int): The user's age. Must be an integer between 5 and 100 (inclusive).
+    - location_country (int): The user's country of residence. Must be an integer corresponding to a country ID in the database.
+    - language (int): The user's preferred language. Must be an integer corresponding to a language ID in the database.
+    - use_category (bool, optional): Whether or not to consider the book category in generating recommendations. Defaults to False.
+    - category (int, optional): The book category to consider in generating recommendations. Required if `use_category` is True.
+    
+Returns:
+    - data (pd.DataFrame): A pandas DataFrame containing the user information for each book in the database. The DataFrame has the following columns:
+        - isbn (int): The book ID.
+        - age (int): The user's age.
+        - location_country (int): The user's country of residence.
+        - language (int): The user's preferred language.
+        - category (int, optional): The book category to consider in generating recommendations. Only present if `use_category` is True.
+    
+Dependencies:
+    - pandas
+    - os
+"""
+
 import pandas as pd
 import os
 
